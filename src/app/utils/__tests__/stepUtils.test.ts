56594096663cc09fbf740ec0e28fdb4fd4db660a
Andre Wolden <wolden.andre@gmail.com>
2020-03-09 15:01:53 +0100

    fix mapping for jest to sif-common libraries

--- src/app/utils/__tests__/stepUtils.test.ts
+++
@@ -1,9 +0,0 @@
-jest.mock('./../../validation/stepValidations', () => {
-    return {
-        welcomingPageIsValid: jest.fn(() => true),
-        opplysningerOmBarnetStepIsValid: jest.fn(() => true),
-        arbeidStepIsValid: jest.fn(() => true),
-        medlemskapStepIsValid: jest.fn(() => true),
-        legeerklæringStepIsValid: jest.fn(() => true)
-    };
-});
