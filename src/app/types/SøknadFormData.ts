10e18ccd293a550b61087702802d0ca9b1d548fb
Frode Hansen <frode.hansen@bekk.no>
2020-03-20 22:36:14 +0100

    sif-common-formik-4.0.7

--- src/app/types/SøknadFormData.ts
+++ src/app/types/SøknadFormData.ts
@@ -1,8 +1,6 @@
 import { Attachment } from '@navikt/sif-common-core/lib/types/Attachment';
 import { Utenlandsopphold } from '@navikt/sif-common-forms/lib//utenlandsopphold/types';
 import { Virksomhet } from '@navikt/sif-common-forms/lib/virksomhet/types';
-import moment from 'moment';
-// import moment from 'moment';
 import { YesOrNo } from 'common/types/YesOrNo';
 import { FraværDelerAvDag, Periode } from '../../@types/omsorgspengerutbetaling-schema';
 
@@ -122,40 +120,11 @@ export const initialValues: SøknadFormData = {
     [SøknadFormField.nettop_startet_selvstendig_frilanser]: YesOrNo.UNANSWERED,
 
     // STEG 3: Periode
-    [SøknadFormField.harPerioderMedFravær]: YesOrNo.NO,
+    [SøknadFormField.harPerioderMedFravær]: YesOrNo.UNANSWERED,
     [SøknadFormField.perioderMedFravær]: [],
-    // [SøknadFormField.harPerioderMedFravær]: YesOrNo.YES,
-    // [SøknadFormField.perioderMedFravær]: [
-    //     {
-    //         fom: moment()
-    //             .subtract(5, 'days')
-    //             .toDate(),
-    //         tom: new Date()
-    //     },
-    //     {
-    //         fom: new Date(),
-    //         tom: moment()
-    //             .add(1, 'days')
-    //             .toDate()
-    //     }
-    //     // mockPeriode
-    // ],
-    // [SøknadFormField.harDagerMedDelvisFravær]: YesOrNo.UNANSWERED,
-    // [SøknadFormField.dagerMedDelvisFravær]: [],
-    [SøknadFormField.harDagerMedDelvisFravær]: YesOrNo.YES,
-    [SøknadFormField.dagerMedDelvisFravær]: [
-        {
-            dato: moment()
-                .add(1, 'day')
-                .toDate(),
-            timer: 9
-        }
-        // {
-        //     dato: new Date(),
-        //     timer: 2
-        // }
-    ],
-    [SøknadFormField.periode_har_vært_i_utlandet]: YesOrNo.NO,
+    [SøknadFormField.harDagerMedDelvisFravær]: YesOrNo.UNANSWERED,
+    [SøknadFormField.dagerMedDelvisFravær]: [],
+    [SøknadFormField.periode_har_vært_i_utlandet]: YesOrNo.UNANSWERED,
     [SøknadFormField.periode_utenlandsopphold]: [],
 
     // STEG 4: Conditional perioder i utlandet
