b16bc4d12f2f167a6db86ff5abf63aeb8cc34542
Frode Hansen <frode.hansen@bekk.no>
2020-03-20 16:02:40 +0100

    Config for step 2

--- src/app/components/søknad/steps/har-utbetalt-de-første-ti-dagene/HarUtbetaltDeFørsteTiDageneStep.tsx
+++ src/app/components/søknad/steps/har-utbetalt-de-første-ti-dagene/HarUtbetaltDeFørsteTiDageneStep.tsx
@@ -4,25 +4,26 @@ import { validateYesOrNoIsAnswered } from '@navikt/sif-common-core/lib/validatio
 import { useFormikContext } from 'formik';
 import CounsellorPanel from 'common/components/counsellor-panel/CounsellorPanel';
 import FormBlock from 'common/components/form-block/FormBlock';
-import { YesOrNo } from 'common/types/YesOrNo';
 import intlHelper from 'common/utils/intlUtils';
 import { StepConfigProps, StepID } from '../../../../config/stepConfig';
 import { SøknadFormData, SøknadFormField } from '../../../../types/SøknadFormData';
 import FormikStep from '../../formik-step/FormikStep';
 import TypedFormComponents from '../../typed-form-components/TypedFormComponents';
+import { HarUtbetaltFørsteTiDagerConfiguestions } from './config';
 
 const HarUtbetaltDeFørsteTiDageneStep = ({ onValidSubmit }: StepConfigProps) => {
     const intl = useIntl();
     const { values } = useFormikContext<SøknadFormData>();
 
-    const { har_utbetalt_ti_dager } = values;
-
+    const visibility = HarUtbetaltFørsteTiDagerConfiguestions.getVisbility(values);
     return (
-        <FormikStep id={StepID.HAR_UTBETALT_DE_FØRST_TI_DAGENE} onValidFormSubmit={onValidSubmit}>
+        <FormikStep
+            id={StepID.HAR_UTBETALT_DE_FØRST_TI_DAGENE}
+            onValidFormSubmit={onValidSubmit}
+            showSubmitButton={visibility.areAllQuestionsAnswered()}>
             <CounsellorPanel>
                 {intlHelper(intl, 'step.har-utbetalt-de-første-ti-dagene.counsellorpanel.content')}
             </CounsellorPanel>
-
             <FormBlock>
                 <TypedFormComponents.YesOrNoQuestion
                     name={SøknadFormField.har_utbetalt_ti_dager}
@@ -30,44 +31,47 @@ const HarUtbetaltDeFørsteTiDageneStep = ({ onValidSubmit }: StepConfigProps) =>
                     validate={validateYesOrNoIsAnswered}
                 />
             </FormBlock>
-
-            {har_utbetalt_ti_dager === YesOrNo.NO && (
-                <>
-                    <FormBlock>
-                        <TypedFormComponents.YesOrNoQuestion
-                            name={SøknadFormField.innvilget_rett_og_ingen_andre_barn_under_tolv}
-                            legend={intlHelper(
-                                intl,
-                                'step.har_utbetalt_de_første_ti_dagene.innvilget_rett_og_ingen_andre_barn_under_tolv.spm'
-                            )}
-                            validate={validateYesOrNoIsAnswered}
-                        />
-                    </FormBlock>
-                    <FormBlock>
-                        <TypedFormComponents.YesOrNoQuestion
-                            name={SøknadFormField.fisker_på_blad_B}
-                            legend={intlHelper(intl, 'step.har_utbetalt_de_første_ti_dagene.fisker_på_blad_B.spm')}
-                            validate={validateYesOrNoIsAnswered}
-                        />
-                    </FormBlock>
-                    <FormBlock>
-                        <TypedFormComponents.YesOrNoQuestion
-                            name={SøknadFormField.frivillig_forsikring}
-                            legend={intlHelper(intl, 'step.har_utbetalt_de_første_ti_dagene.frivillig_forsikring.spm')}
-                            validate={validateYesOrNoIsAnswered}
-                        />
-                    </FormBlock>
-                    <FormBlock>
-                        <TypedFormComponents.YesOrNoQuestion
-                            name={SøknadFormField.nettop_startet_selvstendig_frilanser}
-                            legend={intlHelper(
-                                intl,
-                                'step.har_utbetalt_de_første_ti_dagene.nettop_startet_selvstendig_frilanser.spm'
-                            )}
-                            validate={validateYesOrNoIsAnswered}
-                        />
-                    </FormBlock>
-                </>
+            {visibility.isVisible(SøknadFormField.innvilget_rett_og_ingen_andre_barn_under_tolv) && (
+                <FormBlock>
+                    <TypedFormComponents.YesOrNoQuestion
+                        name={SøknadFormField.innvilget_rett_og_ingen_andre_barn_under_tolv}
+                        legend={intlHelper(
+                            intl,
+                            'step.har_utbetalt_de_første_ti_dagene.innvilget_rett_og_ingen_andre_barn_under_tolv.spm'
+                        )}
+                        validate={validateYesOrNoIsAnswered}
+                    />
+                </FormBlock>
+            )}{' '}
+            {visibility.isVisible(SøknadFormField.fisker_på_blad_B) && (
+                <FormBlock>
+                    <TypedFormComponents.YesOrNoQuestion
+                        name={SøknadFormField.fisker_på_blad_B}
+                        legend={intlHelper(intl, 'step.har_utbetalt_de_første_ti_dagene.fisker_på_blad_B.spm')}
+                        validate={validateYesOrNoIsAnswered}
+                    />
+                </FormBlock>
+            )}
+            {visibility.isVisible(SøknadFormField.frivillig_forsikring) && (
+                <FormBlock>
+                    <TypedFormComponents.YesOrNoQuestion
+                        name={SøknadFormField.frivillig_forsikring}
+                        legend={intlHelper(intl, 'step.har_utbetalt_de_første_ti_dagene.frivillig_forsikring.spm')}
+                        validate={validateYesOrNoIsAnswered}
+                    />
+                </FormBlock>
+            )}{' '}
+            {visibility.isVisible(SøknadFormField.nettop_startet_selvstendig_frilanser) && (
+                <FormBlock>
+                    <TypedFormComponents.YesOrNoQuestion
+                        name={SøknadFormField.nettop_startet_selvstendig_frilanser}
+                        legend={intlHelper(
+                            intl,
+                            'step.har_utbetalt_de_første_ti_dagene.nettop_startet_selvstendig_frilanser.spm'
+                        )}
+                        validate={validateYesOrNoIsAnswered}
+                    />
+                </FormBlock>
             )}
         </FormikStep>
     );
