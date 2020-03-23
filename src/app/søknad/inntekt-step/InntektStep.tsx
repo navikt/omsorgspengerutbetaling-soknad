44aba0b6a71ebe0dc94c89990870b57a3f64f343
Andre Wolden <wolden.andre@gmail.com>
2020-03-23 23:13:18 +0100

    krever at bruker ikke klikker nei på både frilans og selvstendig på inntekt steget

--- src/app/søknad/inntekt-step/InntektStep.tsx
+++ src/app/søknad/inntekt-step/InntektStep.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
-import { useFormikContext } from 'formik';
+import {useFormikContext} from 'formik';
 import Box from 'common/components/box/Box';
-import { StepConfigProps, StepID } from '../../config/stepConfig';
-import { SøknadFormData } from '../../types/SøknadFormData';
+import {StepConfigProps, StepID} from '../../config/stepConfig';
+import {SøknadFormData, SøknadFormField} from '../../types/SøknadFormData';
 import SøknadStep from '../SøknadStep';
 import FrilansFormPart from './components/FrilansFormPart';
 import SelvstendigNæringsdrivendeFormPart from './components/SelvstendigNæringsdrivendePart';
+import {YesOrNo} from "common/types/YesOrNo";
+import FormBlock from "common/components/form-block/FormBlock";
+import {AlertStripeAdvarsel} from "nav-frontend-alertstriper";
+
+const shouldShowSubmitButton = (søknadFormData: SøknadFormData) => {
+    const harHattInntektSomFrilanser: YesOrNo = søknadFormData[SøknadFormField.frilans_harHattInntektSomFrilanser];
+    const harHattInntektSomSN: YesOrNo | undefined = søknadFormData[SøknadFormField.selvstendig_harHattInntektSomSN];
+
+    return !(harHattInntektSomFrilanser === YesOrNo.NO && harHattInntektSomSN === YesOrNo.NO)
+};
 
 const InntektStep = ({ onValidSubmit }: StepConfigProps) => {
     const { values } = useFormikContext<SøknadFormData>();
 
+    const showSubmitButton = shouldShowSubmitButton(values);
     return (
-        <SøknadStep id={StepID.INNTEKT} onValidFormSubmit={onValidSubmit}>
+        <SøknadStep
+            id={StepID.INNTEKT} onValidFormSubmit={onValidSubmit}
+            showSubmitButton={showSubmitButton}
+        >
             <Box margin="l" padBottom="l">
                 <FrilansFormPart formValues={values} />
             </Box>
@@ -19,6 +33,12 @@ const InntektStep = ({ onValidSubmit }: StepConfigProps) => {
             <Box margin="l" padBottom="l">
                 <SelvstendigNæringsdrivendeFormPart formValues={values} />
             </Box>
+
+            {!showSubmitButton && (
+                <FormBlock margin="xxl">
+                    <AlertStripeAdvarsel>Du må velge én av situasjonene over. </AlertStripeAdvarsel>
+                </FormBlock>
+            )}
         </SøknadStep>
     );
 };
