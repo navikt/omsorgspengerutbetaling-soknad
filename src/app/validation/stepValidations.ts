02ef766c69d40cffd20ea26f1e3734ba3960083c
Andre Wolden <wolden.andre@gmail.com>
2020-03-24 21:52:56 +0100

    fullfærer periodeStepIsValid

--- src/app/validation/stepValidations.ts
+++ src/app/validation/stepValidations.ts
@@ -1,7 +1,15 @@
 import { YesOrNo } from 'common/types/YesOrNo';
 import { EgenutbetalingQuestions } from '../søknad/egenutbetaling-step/config';
 import { SituasjonStepQuestions } from '../søknad/situasjon-step/config';
-import { SøknadFormData } from '../types/SøknadFormData';
+import { SøknadFormData, SøknadFormField } from '../types/SøknadFormData';
+import { FraværDelerAvDag, Periode } from '../../@types/omsorgspengerutbetaling-schema';
+import { Utenlandsopphold } from '@navikt/sif-common-forms/lib';
+import {
+    delvisFraværIsValid,
+    harSpesifisertMinimumEnPeriode,
+    oppholdIsValid,
+    perioderIsValid
+} from '../søknad/periode-step/periodeStepConfig';
 
 export const welcomingPageIsValid = ({ harForståttRettigheterOgPlikter }: SøknadFormData): boolean =>
     harForståttRettigheterOgPlikter === true;
@@ -14,8 +22,21 @@ export const egenutbetalingIsValid = (values: SøknadFormData) => {
     return EgenutbetalingQuestions.getVisbility(values).areAllQuestionsAnswered();
 };
 
-export const periodeStepIsValid = ({}: SøknadFormData) => {
-    return true; // TODO: Spesifisere valideringsregler
+export const periodeStepIsValid = (formData: SøknadFormData) => {
+    const harPerioderMedFravær: YesOrNo = formData[SøknadFormField.harPerioderMedFravær];
+    const perioderMedFravær: Periode[] = formData[SøknadFormField.perioderMedFravær];
+    const harDagerMedDelvisFravær: YesOrNo = formData[SøknadFormField.harDagerMedDelvisFravær];
+    const dagerMedDelvisFravær: FraværDelerAvDag[] = formData[SøknadFormField.dagerMedDelvisFravær];
+    const perioderHarVærtIUtlandet: YesOrNo = formData[SøknadFormField.perioder_harVærtIUtlandet];
+    const perioderUtenlandsopphold: Utenlandsopphold[] = formData[SøknadFormField.perioder_utenlandsopphold];
+
+    const isValid: boolean = !!(
+        perioderIsValid(harPerioderMedFravær, perioderMedFravær) &&
+        delvisFraværIsValid(harDagerMedDelvisFravær, dagerMedDelvisFravær) &&
+        oppholdIsValid(perioderHarVærtIUtlandet, perioderUtenlandsopphold) &&
+        harSpesifisertMinimumEnPeriode(perioderMedFravær, dagerMedDelvisFravær)
+    );
+    return isValid;
 };
 
 export const inntektStepIsValid = ({}: SøknadFormData) => {
