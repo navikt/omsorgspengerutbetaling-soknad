932372bf592ff12f6c36d5b9d29f14a9ecddd8bf
Frode Hansen <frode.hansen@bekk.no>
2020-03-25 08:21:31 +0100

    Hide fiskerPåBladB when already answered

--- src/app/søknad/inntekt-step/components/SelvstendigNæringsdrivendePart.tsx
+++ src/app/søknad/inntekt-step/components/SelvstendigNæringsdrivendePart.tsx
@@ -12,6 +12,7 @@ import {
 import { StepID } from '../../../config/stepConfig';
 import { SøknadFormData, SøknadFormField } from '../../../types/SøknadFormData';
 import { Feature, isFeatureEnabled } from '../../../utils/featureToggleUtils';
+import { yesOrNoIsAnswered } from '../../../utils/yesOrNoIsAnswered';
 import SøknadFormComponents from '../../SøknadFormComponents';
 import SøknadTempStorage from '../../SøknadTempStorage';
 
@@ -39,6 +40,7 @@ const SelvstendigNæringsdrivendeFormPart: React.FunctionComponent<Props> = ({ f
                                 addLabel: intlHelper(intl, 'selvstendig.list.leggTilLabel'),
                                 modalTitle: intlHelper(intl, 'selvstendig.dialog.tittel')
                             }}
+                            hideFormFields={{ fiskerErPåBladB: yesOrNoIsAnswered(values.fisker_på_blad_B) }}
                             validate={validateRequiredList}
                             onAfterChange={
                                 isFeatureEnabled(Feature.MELLOMLAGRING)
