9a24d8faae43d93e5dfe48144a5923bf79dd6f5e
Frode Hansen <frode.hansen@bekk.no>
2020-08-10 14:29:42 +0200

    Add eslint

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -8,31 +8,39 @@ require('dotenv').config();
 
 const webpackConfig = {
     entry: {
-        bundle: ['babel-polyfill', `${__dirname}/../../app/App.tsx`]
+        bundle: ['babel-polyfill', `${__dirname}/../../app/App.tsx`],
     },
     output: {
         path: path.resolve(__dirname, './../../../dist'),
         filename: 'js/[name].js',
-        publicPath: `${process.env.PUBLIC_PATH}/dist`
+        publicPath: `${process.env.PUBLIC_PATH}/dist`,
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
         alias: {
             app: path.resolve(__dirname, './../../app'),
-            ['common']: path.resolve(__dirname, './../../../node_modules/@navikt/sif-common-core/lib')
-        }
+            ['common']: path.resolve(__dirname, './../../../node_modules/@navikt/sif-common-core/lib'),
+        },
     },
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/,
-                loader: require.resolve('tslint-loader'),
-                enforce: 'pre'
+                enforce: 'pre',
+                use: [
+                    {
+                        options: {
+                            eslintPath: require.resolve('eslint'),
+                        },
+                        loader: require.resolve('eslint-loader'),
+                    },
+                ],
+                exclude: /node_modules/,
             },
             {
                 test: /\.(ts|tsx)$/,
                 include: [path.resolve(__dirname, './../../app'), path.resolve(__dirname, './../../common')],
-                loader: require.resolve('awesome-typescript-loader')
+                loader: require.resolve('awesome-typescript-loader'),
             },
             {
                 test: /\.less$/,
@@ -40,41 +48,41 @@ const webpackConfig = {
                     fallback: 'style-loader',
                     use: [
                         {
-                            loader: 'css-loader'
+                            loader: 'css-loader',
                         },
                         {
-                            loader: 'postcss-loader'
+                            loader: 'postcss-loader',
                         },
                         {
                             loader: 'less-loader',
                             options: {
                                 globalVars: {
                                     coreModulePath: '"~"',
-                                    nodeModulesPath: '"~"'
-                                }
-                            }
-                        }
-                    ]
-                })
+                                    nodeModulesPath: '"~"',
+                                },
+                            },
+                        },
+                    ],
+                }),
             },
             {
                 test: /\.svg$/,
-                use: 'svg-sprite-loader'
-            }
-        ]
+                use: 'svg-sprite-loader',
+            },
+        ],
     },
     plugins: [
         new CaseSensitivePathsPlugin(),
         new ExtractTextPlugin({
             filename: 'css/[name].css?[hash]-[chunkhash]-[name]',
             disable: false,
-            allChunks: true
+            allChunks: true,
         }),
         new SpriteLoaderPlugin({
-            plainSprite: true
+            plainSprite: true,
         }),
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb|nn|en/)
-    ]
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb|nn|en/),
+    ],
 };
 
 module.exports = webpackConfig;
