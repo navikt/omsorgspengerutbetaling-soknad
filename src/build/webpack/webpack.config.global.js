f5adf67209b01483a40cc9b707124f7cde2dd529
Frode Hansen <frode.hansen@bekk.no>
2021-01-20 09:27:04 +0100

    Update packages

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -1,6 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
@@ -15,10 +14,6 @@ const webpackConfig = {
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
-        alias: {
-            app: path.resolve(__dirname, './../../app'),
-            ['common']: path.resolve(__dirname, './../../../node_modules/@navikt/sif-common-core/lib'),
-        },
     },
     module: {
         rules: [
@@ -37,38 +32,25 @@ const webpackConfig = {
             },
             {
                 test: /\.(ts|tsx)$/,
-                include: [path.resolve(__dirname, './../../app'), path.resolve(__dirname, './../../common')],
+                include: [path.resolve(__dirname, './../../app')],
                 loader: require.resolve('awesome-typescript-loader'),
             },
             {
                 test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                        },
-                        {
-                            loader: 'postcss-loader',
-                        },
-                        {
-                            loader: 'less-loader',
-                        },
-                    ],
-                }),
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
             },
             {
                 test: /\.svg$/,
-                use: 'svg-sprite-loader',
+                loader: 'svg-sprite-loader',
+                options: {},
             },
         ],
     },
     plugins: [
         new CaseSensitivePathsPlugin(),
-        new ExtractTextPlugin({
-            filename: 'css/[name].css?[hash]-[chunkhash]-[name]',
-            disable: false,
-            allChunks: true,
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css?[fullhash]-[chunkhash]-[name]',
+            linkType: 'text/css',
         }),
         new SpriteLoaderPlugin({
             plainSprite: true,
