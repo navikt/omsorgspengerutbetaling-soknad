367554075007d0126e12a7358db7bb399a1d290d
Frode Hansen <frode.hansen@bekk.no>
2021-06-22 17:21:06 +0200

    Flytte feiloppsummering før knapp

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const webpackConfig = {
     entry: {
@@ -18,22 +19,22 @@ const webpackConfig = {
     module: {
         rules: [
             {
-                test: /\.(ts|tsx)$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        options: {
-                            eslintPath: require.resolve('eslint'),
-                        },
-                        loader: require.resolve('eslint-loader'),
-                    },
-                ],
-                exclude: /node_modules/,
+                test: /\.m?jsx?$/,
+                resolve: {
+                    fullySpecified: false,
+                },
             },
             {
                 test: /\.(ts|tsx)$/,
                 include: [path.resolve(__dirname, './../../app')],
-                loader: require.resolve('ts-loader'),
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            experimentalFileCaching: false,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.less$/,
@@ -66,6 +67,11 @@ const webpackConfig = {
         new SpriteLoaderPlugin({
             plainSprite: true,
         }),
+        new ESLintPlugin({
+            extensions: ['ts', 'tsx'],
+            failOnWarning: false,
+        }),
+
     ],
 };
 
