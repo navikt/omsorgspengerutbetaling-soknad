165e6ae38599ca89b8b975aed3b0a538e83b7cbd
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-10-18 14:08:14 +0200

    Migrering WebpackDevServer til v4

--- src/build/scripts/start-dev-no-decorator.js
+++ src/build/scripts/start-dev-no-decorator.js
@@ -6,7 +6,6 @@ const configureDevServer = require('../webpack/devserver.config');
 require('dotenv').config();
 
 const compiler = webpack(webpackConfig);
-const server = new WebpackDevServer(compiler, configureDevServer({}));
 
-let PORT = 8080;
-server.listen(PORT, '127.0.0.1', () => console.log(`Started server on http://localhost:${PORT}`));
+const server = new WebpackDevServer(configureDevServer({}), compiler);
+server.start();
