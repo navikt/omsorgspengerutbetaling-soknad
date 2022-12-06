5d81ecca3ebfba3e1a30af96ad41fa5a2d260bf6
Frode Hansen <frode.hansen@bekk.no>
2022-12-06 14:15:30 +0100

    Tekstjusteringer

--- src/app/pages/velkommen-page/velkommenPageMessages.ts
+++ src/app/pages/velkommen-page/velkommenPageMessages.ts
@@ -8,14 +8,14 @@ export const velkommenPageMessages: MessageFileFormat = {
         'page.velkommen.guide.tekst.1.2': 'selvstendig næringsdrivende eller frilanser',
         'page.velkommen.guide.tekst.1.3': ' og skal søke om utbetaling av omsorgspenger for bruk av omsorgsdager.',
         'page.velkommen.guide.tekst.2.1':
-            'Hvis du er ansatt i eget AS regnes du som arbeidstaker, da skal du eller din regnskapsfører ',
-        'page.velkommen.guide.tekst.2.2': 'sende inntektsmelding til NAV.',
+            'Hvis du er ansatt i eget AS regnes du som arbeidstaker og du skal ikke bruke denne søknaden. For å få utbetalt omsorgspenger når du er ansatt i eget AS skal du eller din regnskapsfører ',
+        'page.velkommen.guide.tekst.2.2': 'sende inntektsmelding til NAV',
         'page.velkommen.guide.tekst.2.3': '.',
 
         'page.velkommen.omSøknaden.tittel': 'Om søknaden',
         'page.velkommen.omSøknaden.1': 'Du får veiledning underveis i søknaden om hva du skal fylle ut, og hvordan.',
         'page.velkommen.omSøknaden.2':
-            'Vi tar vare på svarene dine i 72 timer. Så, hvis du innenfor den tiden for eksempel vil ta en pause eller blir automatisk logget ut, fortsetter du der du var når du kommer tilbake.',
+            'Vi tar vare på svarene dine i 72 timer. Hvis du innenfor den tiden for eksempel vil ta en pause eller blir automatisk logget ut, fortsetter du der du var når du kommer tilbake.',
         'page.velkommen.omSøknaden.3':
             'Du må svare på alle spørsmålene for å kunne gå videre. Hvis du mangler etterspurt dokumentasjon, kan du ettersende det så snart du kan.',
         'page.velkommen.omSøknaden.4': 'Om hvordan vi innhenter opplysninger om deg',
